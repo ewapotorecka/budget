933fb776ae0e38baced78d1df5c720c719931dda
Ewa Potorecka <ewa.potorecka@gmail.com>
2020-12-04 13:38:26 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Budget from './components/Budget';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	const expensesStorage = JSON.parse( localStorage.getItem( 'expenses' ) );
+	const incomesStorage = JSON.parse( localStorage.getItem( 'incomes' ) );
+
+	return (
+		<div className="App">
+			<Budget expenses={ expensesStorage ? expensesStorage : [] } incomes={ incomesStorage ? incomesStorage : [] }/>
+		</div>
   );
 }
 
